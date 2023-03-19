f381f549827ee664ca279fbf000f0ec7cbe548be
Ernest <ernest.wambugu@student.moringaschool.com>
2023-03-19 15:55:47 +0300

    Completed the speed detector

--- grade-generator/index.js
+++ grade-generator/index.js
@@ -1,18 +1,26 @@
-//Promt the user to input a number for their grade between 0 and 100
-let studentMarks = prompt("Please enter the student's marks between 0 and 100:");
-
-// Convert the input to a number
-studentMarks = Number(studentMarks);
-
-// Check the grade based on the marks and return a  Grade
-if (studentMarks > 79) {
-  console.log("Grade A");
-} else if (studentMarks >= 60 && studentMarks <= 79) {
-  console.log("Grade B");
-} else if (studentMarks >= 49 && studentMarks <= 59) {
-  console.log("Grade C");
-} else if (studentMarks >= 40 && studentMarks <= 49) {
-  console.log("Grade D");
-} else if (studentMarks < 40 ){
-  console.log("Grade E");
+const studentMark=document.getElementById(
+    'studentMark'
+);
+const showGrade=document.getElementById(
+    'showGrade'
+)
+function getGrade(){
+const mark=parseInt(studentMark.value);
+let grade;
+if(mark>=80){
+    grade='A';
+}
+else if(mark>=60){
+    grade='B';
+}
+else if (mark>=50){
+    grade='C';
+}
+else if(mark>=40) {
+     grade='D';
+}
+else{grade='E'
+}
+console.log(grade)
+showGrade.innerText=`Your grade is, ${grade}`;
 }
